ef86bad118ba394659d19974c2e0465d0e8b94ac
Millie Walsh <millie@primeacademy.io>
2016-09-25 21:20:23 -0500

    add test dummy get route

--- routers/pets.js
+++ routers/pets.js
@@ -3,7 +3,25 @@ var express = require('express');
 var router = express.Router();
 
 // require the Pets Model
-var Pets = require('../models/petModel');
+var Pet = require('../models/petModel');
+
+router.get('/test', function(req, res) {
+  var dummyPet = new Pet({
+    name: 'nyan',
+    animal_type: 'cat',
+    age: 42
+  });
+
+  dummyPet.save(function(err) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      // created
+      res.sendStatus(201);
+    }
+  });
+});
 
 /* Gets all pets */
 router.get('/', function(req, res) {
