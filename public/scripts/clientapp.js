3ca8fdc73a6ff529fff918702d1af2e69cde20bd
Millie Walsh <millie@primeacademy.io>
2016-09-25 23:07:46 -0500

    add post and some styling

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -2,7 +2,7 @@ console.log('sourced clientapp.js');
 
 var myApp = angular.module( 'myApp', [] );
 
-myApp.controller('petViewController', ['$scope', '$http', function($scope, $http) {
+myApp.controller('viewPetController', ['$scope', '$http', function($scope, $http) {
   $http({
     method: 'GET',
     url: '/pets'
@@ -12,4 +12,28 @@ myApp.controller('petViewController', ['$scope', '$http', function($scope, $http
   }, function errorCallback(response) {
     console.log('err');
   });
+}]);
+
+myApp.controller('addPetController', ['$scope', '$http', function($scope, $http) {
+  $scope.addPet = function(name, animalType, age, image) {
+    var dataToSend ={
+      name: name,
+      animal_type: animalType,
+      age: age,
+      image: image
+    };
+
+    console.log('data to send =', dataToSend);
+
+    //ajax call
+    $http({
+      method: 'POST',
+      url: '/pets',
+      data: dataToSend
+    }).then(function successCallback(response) {
+      console.log('post resp =', response);
+    }, function errorCallback(response) {
+      console.log('err');
+    });
+  };
 }]);
