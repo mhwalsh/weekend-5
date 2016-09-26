8be28b944221b7d00de9c786ea594aaae3eeddf6
Millie Walsh <millie@primeacademy.io>
2016-09-26 00:11:26 -0500

    ng init

--- public/scripts/controllers/viewPetController.js
+++ public/scripts/controllers/viewPetController.js
@@ -1,11 +1,13 @@
 myApp.controller('viewPetController', ['$scope', '$http', function($scope, $http) {
-  $http({
-    method: 'GET',
-    url: '/pets'
-  }).then(function successCallback(response) {
-    $scope.pets = response.data;
-    console.log('got em =', response);
-  }, function errorCallback(response) {
-    console.log('err');
-  });
+  $scope.getPets = function() {
+    $http({
+      method: 'GET',
+      url: '/pets'
+    }).then(function successCallback(response) {
+      $scope.pets = response.data;
+      console.log('got em =', response);
+    }, function errorCallback(response) {
+      console.log('err');
+    });
+  };
 }]);
